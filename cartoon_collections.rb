c271b78559f2718bc6e2210ef44e00c4ba2123de
Edward Castillo <castilloedwarda@gmail.com>
2019-11-15 05:38:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,14 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)# code an argument here
+  i = 0 
+  if array.any? { |i| i.length > 4 }
+    return true 
+  else 
+    return false 
+    i += 1
+  end 
 end
 
 def find_the_cheese# code an argument here
